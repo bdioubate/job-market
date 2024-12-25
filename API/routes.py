77b36389910ce56add48d86b3fd2e5bf9f233dfc
bdioubate <bangalidioubate@gmail.com>
2024-12-25 17:28:55 +0100

    update API request

--- API/routes.py
+++ API/routes.py
@@ -10,7 +10,8 @@ router = APIRouter()
 def get_custom_data(db: Session = Depends(get_db)):
     query = """
         SELECT rome_code, A.rome_label, contract_type, experience_required,
-               experience_required_months, departement, A.code_postal
+               experience_required_months, departement, A.code_postal,
+               date_creation, calculated_salary
         FROM jm_job A
         LEFT JOIN jm_rome B ON A.rome_label = B.rome_label
         LEFT JOIN jm_code_postaux C ON A.code_postal = C.code_postal;
